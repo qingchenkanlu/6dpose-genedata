6b2f4c5e5a275356428951d3b2731550cbcd4aea
qingchenkanlu <44363924+qingchenkanlu@users.noreply.github.com>
2019-11-24 22:20:47 +0800

    Add files via upload

---
+++ kps/pcl-sift/build/CMakeFiles/pcl-sift.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcl-sift.dir/main.cpp.o"
+  "pcl-sift.pdb"
+  "pcl-sift"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcl-sift.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
